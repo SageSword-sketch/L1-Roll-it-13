764531bf6919b95f70ceda24f376f9c9ab48229f
SageSword-sketch <singha4349@masseyhigh.school.nz>
2026-02-11 12:21:58 +1300

    simple yes / no checker (incomplete)

---
+++ C_01_yes_no_basic.py
@@ -0,0 +1,10 @@
+want_instruction =input("do you want to see the instruction? ").lower()
+
+# check the user says yes / no
+if want_instruction == "yes" or want_instruction == "y"
+   print("y")
+elif want_instructions == "no":or want instructions == "n":
+     print("you said no"
+else:
+     print("please enter yes / no")
+
